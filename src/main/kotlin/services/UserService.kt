c22bf1640b3e20e98d80fe7993979a9ab816e8b7
Alexandr Daineko <alexandr.daineko@gmail.com>
2019-04-17 00:52:15 +0300

    message

--- src/main/kotlin/services/UserService.kt
+++ src/main/kotlin/services/UserService.kt
@@ -3,20 +3,19 @@ package services
 import dao.DBConnector.DatabaseFactory
 import dao.entitys.User
 import dao.entitys.Users
-import org.jetbrains.exposed.sql.Query
 import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class UserService {
-    public fun getAllUsers(): ResultRow {
+    public fun getAllUsers() {
         DatabaseFactory.connect()
 
-        val jamesList = transaction {
-            Users.selectAll()
-        }
+        val user = User(3, "dasf", "asdfgfsd", "aeadfvaertvaervadf")
 
-        return jamesList.first()
+        transaction {
+            Users.insert { user }
+        }
     }
 }
